5302afc396c830dc0384581244c7447060cfbc4f
kevin <gch1018511235@hotmail.com>
2020-11-27 14:35:58 +0900

    add support pic

---
+++ lib/model/post.dart
@@ -0,0 +1,59 @@
+class Post {
+  const Post({this.title, this.author, this.imageUrl});
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title: 'eweb',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://eweb.co.jp/img/p05-logo01.png',
+  ),
+  Post(
+    title: 'nsupport',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://eweb.co.jp/img/p05-logo05.png',
+  ),
+  Post(
+    title: 'nsupport',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://eweb.co.jp/img/p05-logo03.png',
+  ),
+  Post(
+    title: 'nsupport',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://eweb.co.jp/img/p05-graphicBG2.png',
+  ),
+  Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img.ivsky.com/img/tupian/pre/201402/18/pikachu-001.jpg',
+  ),
+  Post(
+    title: 'Contained',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.net/images/contained.jpg',
+  ),
+  Post(
+    title: 'Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.net/images/dragon.jpg',
+  ),
+  Post(
+    title: 'Free Hugs',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.net/images/free_hugs.jpg',
+  ),
+  Post(
+    title: 'Gravity Falls',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.net/images/gravity-falls.png',
+  ),
+  Post(
+    title: 'Icecream Truck',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.net/images/icecreamtruck.png',
+  ),
+];
